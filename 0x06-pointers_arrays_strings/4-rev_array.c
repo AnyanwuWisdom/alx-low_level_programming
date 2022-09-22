802ffaa54cf731eece90e2c2de6523f6de0f14e1
AnyanwuWisdom <Anyanwuwisdomobinna@gmail.com>
2022-09-22 14:07:40 -0700

    more arrays and pointers

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,46 +1,18 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_buffer - print a buffer
- * @b: the buffer to print
- * @size: the number of bytes to print
- *
- * Description: This function prints the content of of size bytes of the
- * buffer pointed to by b. 10 bytes are printed per line, with each line
- * prefixed with the position of the first byte, starting at 0. Each line
- * shows the heaxadecimal content of the buffer 2 bytes at a time, with
- * each pair separated by a space, followed by the content of the buffer
- * with non-printable characters shown as a `.'.
+ * reverse_array - reverse an array of integers
+ * @a: the array to reverse
+ * @n: the size of the array
  *
  * Return: void
  */
-void print_buffer(char *b, int size)
+void reverse_array(int *a, int n)
 {
-int b_pos;
-int l_pos;
-
-for (b_pos = 0; b_pos < size; b_pos += 10)
+for ( ; --n > 0; ++a, --n)
 {
-printf("%08x: ", b_pos);
-for (l_pos = 0; l_pos < 10; ++l_pos)
-{
-if (b_pos + l_pos < size)
-printf("%02x", b[b_pos + l_pos]);
-else
-printf("  ");
-if (l_pos % 2)
-putchar(' ');
+*a ^= a[n];
+a[n] ^= *a;
+*a ^= a[n];
 }
-for (l_pos = 0; l_pos < 10 && b_pos + l_pos < size; ++l_pos)
-{
-if (b[b_pos + l_pos] < 32 || b[b_pos + l_pos] > 126)
-putchar('.');
-else
-putchar(b[b_pos + l_pos]);
-}
-if (b_pos + l_pos < size)
-putchar('\n');
-}
-putchar('\n');
 }
